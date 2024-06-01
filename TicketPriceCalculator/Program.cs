f62656b891ea189ad5e270a13d8a248966d41500
unknown <iowusu033@st.ug.edu.gh>
2024-06-01 13:28:15 +0000

    Addded Ticket Price Calculator console application

--- TicketPriceCalculator/Program.cs
+++ TicketPriceCalculator/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age to view the price of ticket: ");
+            string? input = Console.ReadLine();  
+            int age;
+
+            if (int.TryParse(input, out age) && age >= 0)
+            {
+                int ticketPrice = (age <= 12 || age >= 65) ? 7 : 10;
+
+                Console.WriteLine($"The ticket price for age {age} is GHC{ticketPrice}.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid age.");
+            }
+        }
+    }
+}
+
