993e6dd6f962f317f6aef63d98bb601411fa3996
unknown <iowusu033@st.ug.edu.gh>
2024-06-01 12:59:17 +0000

    Added Grade Calculator console application

---
+++ GradeCalculator/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace GradeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a numerical grade within range(0-100): ");
+            string input = Console.ReadLine() ?? string.Empty;
+            int grade;
+
+            if (int.TryParse(input, out grade) && grade >= 0 && grade <= 100)
+            {
+                string letterGrade;
+
+                if (grade >= 90)
+                {
+                    letterGrade = "A";
+                }
+                else if (grade >= 80)
+                {
+                    letterGrade = "B";
+                }
+                else if (grade >= 70)
+                {
+                    letterGrade = "C";
+                }
+                else if (grade >= 60)
+                {
+                    letterGrade = "D";
+                }
+                else
+                {
+                    letterGrade = "F";
+                }
+
+                Console.WriteLine($"The letter grade for {grade} is {letterGrade}.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number between 0 and 100.");
+            }
+        }
+    }
+}
